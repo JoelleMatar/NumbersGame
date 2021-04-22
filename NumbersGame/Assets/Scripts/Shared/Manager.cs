bed8a7e0e9af0b4ef9dc3b490cc5fea8f1730c8a
RachaHachem <rachahachm@gmail.com>
2021-04-22 23:18:22 +0300

    Scripts for cover scene & ex1

---
+++ NumbersGame/Assets/Scripts/Shared/Manager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public static bool nb3 = true, nb5 = true, nb7 = true, nb10 = true;
+
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    moving = false;
+    //}
+    public void playClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitClick()
+    {
+        Application.Quit();
+        Debug.Log("Application quit");
+    }
+
+    public void restartClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void homeClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void PlayMusic()
+    {
+        //DontDestroy._audioSource.UnPause();
+        DontDestroy._audioSource.mute = !DontDestroy._audioSource.mute;
+        DontDestroy.isMuted = false;
+    }
+
+    public void StopMusic()
+    {
+        //DontDestroy._audioSource.Pause();
+        DontDestroy._audioSource.mute = !DontDestroy._audioSource.mute;
+        DontDestroy.isMuted = true;
+    }
+    
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
---
+++ NumbersGame/Assets/Scripts/ex1/Number5.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number5 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform objectPlace;
+    private Vector2 initialPos;
+    private float deltaX, deltaY;
+    public static bool locked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreLayerCollision(5, 5);
+        initialPos = transform.position;
+        locked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.IgnoreLayerCollision(5, 5);
+        if (Input.touchCount > 0 && !locked && Manager.nb5)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                        Manager.nb3 = false;
+                        Manager.nb7 = false;
+                        Manager.nb10 = false;
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    Physics.IgnoreLayerCollision(5, 5);
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                    break;
+
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - objectPlace.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - objectPlace.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(objectPlace.position.x, objectPlace.position.y);
+                        locked = true;
+                        Manager.nb3 = true;
+                        Manager.nb7 = true;
+                        Manager.nb10 = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(initialPos.x, initialPos.y);
+                        Manager.nb3 = true;
+                        Manager.nb7 = true;
+                        Manager.nb10 = true;
+                    }
+                    break;
+            }
+        }
+    }
+}
